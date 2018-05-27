55892cbf13d649b444945765453a3416b3462a52
Nilesh Magan <NileshMagan>
2018-05-28 09:24:46 +1200

    Bools fixed? Code compiles

--- cruiseregulation.h
+++ cruiseregulation.h
@@ -1,5 +1,8 @@
+typedef int boolean;
 // Header file
-float saturateThrottle(float throttleIn, bool* saturate);
-float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed);
+
+
+float saturateThrottle(float throttleIn, boolean saturate);
+float regulateThrottle(boolean isGoingOn, float cruiseSpeed, float vehicleSpeed);
 int outCount();
 
---
+++ cruiseregulation_data.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cruiseregulation.h"
+
+
+int cnt=0;
+
+int outCount(){
+  return ++cnt;
+}
+
+
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: throttleIn - throttle input
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float saturateThrottle(float throttleIn, boolean saturate)
+{
+	static const float THROTTLESATMAX = 45.0;
+	if (throttleIn > THROTTLESATMAX) {
+		*saturate = 1;
+		return THROTTLESATMAX;
+	}
+	else if (throttleIn < 0) {
+		*saturate = 1;
+		return 0;
+	}
+	else {
+		*saturate = 0;
+		return throttleIn;
+	}
+}
+
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: isGoingOn - true if the cruise control has just gone into the ON state 
+                        from another state; false otherwise
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float regulateThrottle(boolean isGoingOn, float cruiseSpeed, float vehicleSpeed)
+{
+	static const float KP = 8.113;
+	static const float KI = 0.5;
+	static boolean saturate = 1;
+	static float iterm = 0;
+	
+	if (isGoingOn) {
+		iterm = 0;	// reset the integral action
+		saturate = 1;	
+	}
+	float error = cruiseSpeed - vehicleSpeed;
+	float proportionalAction = error * KP;
+	if (saturate)
+		error = 0;	// integral action is hold when command is saturated
+	iterm = iterm + error;
+	float integralAction = KI * iterm;
+	return saturateThrottle(proportionalAction + integralAction, &saturate);
+}
